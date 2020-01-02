9612b536d75b4c38771af219fe8d94b0d2e25612
Sylvia Lin <SylviaLin@via.com.tw>
2020-01-02 17:08:53 +0800

    Remove garbage characters from examples which leads to compile erros.

--- SmartSensor/examples/sensor_pantilt_hwserial/sensor_pantilt_hwserial.ino
+++ SmartSensor/examples/sensor_pantilt_hwserial/sensor_pantilt_hwserial.ino
@@ -1,4 +1,4 @@
-﻿#include <SmartSensor.h>
+#include <SmartSensor.h>
 #include <Servo.h>
 
 #define IMG_CENTER_X  500
@@ -37,88 +37,88 @@ int b = 0;
 
 void setup()
 {
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  pinMode(servoXPin, OUTPUT);
-  pinMode(servoYPin, OUTPUT);
+	pinMode(rxPin, INPUT);
+	pinMode(txPin, OUTPUT);
+	pinMode(servoXPin, OUTPUT);
+	pinMode(servoYPin, OUTPUT);
 
-  ss.begin();
-  servo_x.attach(servoXPin);
-  servo_y.attach(servoYPin);
-  servo_x.write(DEG_CENTER);
-  servo_y.write(DEG_CENTER);
+	ss.begin();
+	servo_x.attach(servoXPin);
+	servo_y.attach(servoYPin);
+	servo_x.write(DEG_CENTER);
+	servo_y.write(DEG_CENTER);
 }
 
 void loop()
 {
-  String s = "";
-  if(ss.isDetected())
-  {
-    nFuncID = ss.getFuncID();
-    nTypeID = ss.getTypeID();
-    nPosX = ss.getPosX();
-    nPosY = ss.getPosY();
-    nPosH = ss.getH();
-    nPosW = ss.getW();
+	String s = "";
+	if(ss.isDetected())
+	{
+		nFuncID = ss.getFuncID();
+		nTypeID = ss.getTypeID();
+		nPosX = ss.getPosX();
+		nPosY = ss.getPosY();
+		nPosH = ss.getH();
+		nPosW = ss.getW();
 
-    nCenterX = nPosX + nPosW/2;
+		nCenterX = nPosX + nPosW/2;
 
-    if (nCenterX > IMG_CENTER_X + 60)
-    {
-      diffX = nCenterX - IMG_CENTER_X;
-      moveX = map(diffX, 60,IMG_CENTER_X,0,20);
+		if (nCenterX > IMG_CENTER_X + 60)
+		{
+			diffX = nCenterX - IMG_CENTER_X;
+			moveX = map(diffX, 60,IMG_CENTER_X,0,20);
 
-      b = servo_y.read();
-      if (b < DEG_CENTER)
-      {
-        a = servo_x.read() - moveX;
-        if (a < MIN_DEG_X) a = MIN_DEG_X;
-      }
-      else
-      {
-        a = servo_x.read() + moveX;
-        if (a > MAX_DEG_X) a = MAX_DEG_X;
-      }
-      servo_x.write(a);
-    }
-    else if (nCenterX < IMG_CENTER_X - 60)
-    {
-      diffX = IMG_CENTER_X - nCenterX;
-      moveX = map(diffX, 60,IMG_CENTER_X,0,20);
+			b = servo_y.read();
+			if (b < DEG_CENTER)
+			{
+				a = servo_x.read() - moveX;
+				if (a < MIN_DEG_X) a = MIN_DEG_X;
+			}
+			else
+			{
+				a = servo_x.read() + moveX;
+				if (a > MAX_DEG_X) a = MAX_DEG_X;
+			}
+			servo_x.write(a);
+		}
+		else if (nCenterX < IMG_CENTER_X - 60)
+		{
+			diffX = IMG_CENTER_X - nCenterX;
+			moveX = map(diffX, 60,IMG_CENTER_X,0,20);
 
-      b = servo_y.read();
-      if (b < DEG_CENTER)
-      {
-        a = servo_x.read() + moveX;
-        if (a > MAX_DEG_X) a = MAX_DEG_X;
-      }
-      else
-      {
-        a = servo_x.read() - moveX;
-        if (a < MIN_DEG_X) a = MIN_DEG_X;
-      }
-      servo_x.write(a);
-    }
+			b = servo_y.read();
+			if (b < DEG_CENTER)
+			{
+				a = servo_x.read() + moveX;
+				if (a > MAX_DEG_X) a = MAX_DEG_X;
+			}
+			else
+			{
+				a = servo_x.read() - moveX;
+				if (a < MIN_DEG_X) a = MIN_DEG_X;
+			}
+			servo_x.write(a);
+		}
 
-    nCenterY = nPosY + nPosH/2;
+		nCenterY = nPosY + nPosH/2;
 
-    if (nCenterY > IMG_CENTER_Y + 80)
-    {
-      diffY = nCenterY - IMG_CENTER_Y;
-      moveY = map(diffY, 80,IMG_CENTER_Y,0,12);
-      b = servo_y.read() - moveY;
-      if (b < MIN_DEG_Y) b = MIN_DEG_Y;
-      servo_y.write(b);
-    }
-    else if (nCenterY < IMG_CENTER_Y - 80)
-    {
-      diffY = IMG_CENTER_Y - nCenterY;
-      moveY = map(diffY, 80,IMG_CENTER_Y,0,12);
-      b = servo_y.read() + moveY;
-      if (b > MAX_DEG_Y) b = MAX_DEG_Y;
-      servo_y.write(b);
-    }
-  }
+		if (nCenterY > IMG_CENTER_Y + 80)
+		{
+			diffY = nCenterY - IMG_CENTER_Y;
+			moveY = map(diffY, 80,IMG_CENTER_Y,0,12);
+			b = servo_y.read() - moveY;
+			if (b < MIN_DEG_Y) b = MIN_DEG_Y;
+			servo_y.write(b);
+		}
+		else if (nCenterY < IMG_CENTER_Y - 80)
+		{
+			diffY = IMG_CENTER_Y - nCenterY;
+			moveY = map(diffY, 80,IMG_CENTER_Y,0,12);
+			b = servo_y.read() + moveY;
+			if (b > MAX_DEG_Y) b = MAX_DEG_Y;
+			servo_y.write(b);
+		}
+	}
 
-    delay(20);
+	delay(20);
 }
